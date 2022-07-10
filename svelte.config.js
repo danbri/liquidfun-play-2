60163f47f653650ce785ab0411f79eace3887c41
Dan Brickley <danbri@danbri.org>
2022-07-10 16:51:19 +0100

    attempting to use sveltekit instead of snowpack, which is no longer maintained.

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-const autoPreprocess = require('svelte-preprocess')
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
-module.exports = {
-  preprocess: autoPreprocess({
-    defaults: {
-      script: 'typescript'
-    }
-  })
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: preprocess(),
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],       clearScreen: false
+};
+
+export default config;
